ae695039a7ec4efd6d4c44f66355b8bdb037b1c5
streq <strequena@gmail.com>
2023-07-17 14:05:45 -0300

    NOW clock added

--- expected_finish.gd
+++ expected_finish.gd
@@ -6,6 +6,8 @@ export var timer_path : NodePath
 onready var timer = get_node(timer_path)
 export var goal_path : NodePath
 onready var goal = get_node(goal_path)
+export var now_path : NodePath
+onready var now = get_node(now_path)
 
 
 func _process(delta: float) -> void:
@@ -15,10 +17,6 @@ func _ready() -> void:
 	timer.connect("started",self,"set_process",[false])
 	timer.connect("stopped",self,"set_process",[true])
 	goal.connect("updated",self,"update_time")
+	
 func update_time():
-	var msecs_now = Global.from_text(Time.get_time_string_from_system())
-	#for subsecond precision, sucks I know
-	var seconds = Time.get_unix_time_from_system()
-	msecs_now += int(seconds*1000.0)%1000
-	var msecs_remaining = msecs_now + remainder.msec
-	text = Global.to_text_wrap_hours(msecs_remaining)
+	text = Global.to_text_wrap_hours(now.msec + remainder.msec)
